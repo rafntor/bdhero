30e3e99107fb657e17f1fb976a84440d4b5d98a7
Tore Fuglestad <tore@fuglestad.no>
2019-02-11 21:59:29 +0100

    download & extract ffmpeg

---
+++ src/Plugins/FFmpegMuxerPlugin/FFMpegExtract.ps1
@@ -0,0 +1,16 @@
+﻿
+cd $PSScriptRoot
+
+$ToolPath = $args[0]
+$FFMPEGZIP = Get-ChildItem -path "$ToolPath\ffmpeg*.zip"
+$FFMPEGZIP = $FFMPEGZIP[0] # get first, if multiple
+
+$SEVENZIP = Get-ChildItem -path "..\..\7z*.exe" -recurse
+$SEVENZIP = $SEVENZIP[0] # get first, if multiple
+
+#write-host $FFMPEGZIP 
+#write-host $FFMPEGZIP.BaseName
+$FFMPEGEXE = $FFMPEGZIP.BaseName + "\bin\ffmpeg.exe"
+#write-host  $FFMPEGEXE
+
+& $SEVENZIP e -y $FFMPEGZIP $FFMPEGEXE # extract just ffmpeg.exe
