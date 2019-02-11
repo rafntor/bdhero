8e6e2931a31fc32a6ab424e2ed0806b15ddb25b8
Tore Fuglestad <tore@fuglestad.no>
2019-02-11 19:57:00 +0100

    download mkvtoolnix ; extract mkvmerge.exe

---
+++ src/Plugins/MkvMergeMuxerPlugin/MkvToolExtract.ps1
@@ -0,0 +1,11 @@
+﻿
+cd $PSScriptRoot
+
+$ToolPath = $args[0]
+$MKVTOOLNIX = Get-ChildItem -path "$ToolPath\mkvtoolnix*.7z"
+$MKVTOOLNIX = $MKVTOOLNIX[0] # get first, if multiple
+
+$SEVENZIP = Get-ChildItem -path "..\..\7z*.exe" -recurse
+$SEVENZIP = $SEVENZIP[0] # get first, if multiple
+
+& $SEVENZIP e -y $MKVTOOLNIX mkvtoolnix/mkvmerge.exe # extract just mkvmerge.exe
