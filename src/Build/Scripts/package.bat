b38fcace40aebf1b6ec29f346f953dda0d26f990
Tore Fuglestad <tore@fuglestad.no>
2019-04-17 16:31:36 +0200

    drop signing & setup

--- src/Build/Scripts/package.bat
+++ src/Build/Scripts/package.bat
@@ -13,7 +13,7 @@ xcopy /Y %ArtifactsPath%\Installer\ProgramFiles\*.exe %ArtifactsPath%\Portable\
 REM "%ProgramFiles%\BitRock InstallBuilder Enterprise 8.6.0\bin\builder-cli.exe" build "Installer.xml" windows
 
 REM Use `sed` to redact the path to and password for the private key file used to digitally sign EXEs
-call %InnoSetup%\iscc "/sCustom=%SignTool%\signtool.exe $p" Build\InnoSetup\setup.iss | %unxutils%\sed "s/\/[df] .*/[REDACTED]/gi"
+REM call %InnoSetup%\iscc "/sCustom=%SignTool%\signtool.exe $p" Build\InnoSetup\setup.iss | %unxutils%\sed "s/\/[df] .*/[REDACTED]/gi"
 
 REM %SevenZip%\7za a -sfx7z.sfx -r "%SfxPath%" %ArtifactsPath%\Portable\*
 REM %SevenZip%\7za a -r "%SevenZipPath%" %ArtifactsPath%\Portable\*
