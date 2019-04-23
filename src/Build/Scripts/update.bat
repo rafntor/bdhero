0f0f5ce2b53c9008dc6da843b25eb4688764d9c3
Tore Fuglestad <tore@fuglestad.no>
2019-04-23 20:58:17 +0200

    drop 'umgen' as it is now broken and not needed

--- src/Build/Scripts/update.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-REM %CD% = C:\Projects\BDHero\src
-
-call Build\Scripts\tools.bat
-
-Build\Tools\UMGen --mirror "%MirrorUrl%" --windows --setup "%SetupPath%" --portable "%ZipPath%" --output update.json
