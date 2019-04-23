c026525c6476474b9bd908beef6f612b6385de4d
Tore Fuglestad <tore@fuglestad.no>
2019-04-23 21:06:29 +0200

    bump to new version range

--- src/Core/BDHeroGUI/Properties/AssemblyInfo.cs
+++ src/Core/BDHeroGUI/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("BDHero")]
 [assembly: AssemblyProduct("BDHero")]
-[assembly: AssemblyCopyright("Copyright © 2012-2014 Andrew C. Dvorak")]
+[assembly: AssemblyCopyright("Copyright © 2012-2019 Andrew C. Dvorak, Tore Fuglestad")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.1.1")]
-[assembly: AssemblyFileVersion("0.9.1.1")]
+[assembly: AssemblyVersion("0.42.0.0")]
+[assembly: AssemblyFileVersion("0.42.0.0")]
